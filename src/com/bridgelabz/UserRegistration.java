86448704cd3f984af569b598e0a2883d815ab89b
nehachougule1998 <nchougule734@gmail.com>
2022-03-31 18:46:40 +0530

    Added

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -4,58 +4,46 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	
-public static void main(String[] args) {
-	Scanner scan = new Scanner(System.in);
-	System.out.println("Enter your first name :");
-	String firstName = scan.nextLine();
-	System.out.println("Enter your last name :");
-	String lastName = scan.nextLine();
-	System.out.println("Enter your Email ID :");
-	String emailId = scan.nextLine();
-	System.out.println("Enter your Mobile Number:");
-	String mobileNumber = scan.nextLine();
-	System.out.println("Enter password:");
-	String password = scan.nextLine();
-	
-	System.out.println("first name is : " +firstName);
-	System.out.println("first name is valid ? " + firstName(firstName));
-	
-	System.out.println("last name is : " +lastName);
-	System.out.println("last name is valid ? " + lastName(lastName));
-	
-	System.out.println("Email ID is : " +emailId);
-	System.out.println("Email ID valid ? " + emailId(emailId));
-	
-	System.out.println("Mobile No is : " +mobileNumber);
-	System.out.println("Mobile No valid ? " + mobileNumber(mobileNumber));
-	
-	System.out.println("Password valid ? " + password(password));
-	
-}
 
-public static boolean firstName(String firstName) {
-	return firstName.matches("[A-Z]{1}[a-z]{3}");	
-}
-
-public static boolean lastName(String lastName) {
-	return lastName.matches("[A-Z]{1}[a-z]{3}");
-}
-
-public static boolean emailId(String emailId) {
-	return emailId.matches("[abc](.+){1}[A-Za-z]*+@{1}[bl](.+)[co](.+)[in]$");
-}
-
-public static boolean mobileNumber(String mobileNumber) {
-	return mobileNumber.matches("^(91)\\s{1}[7-9]{1}[0-9]{9}$");
-}
-
-public static boolean password(String password) {
-	String pattern = "(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}";
-	if(password.matches(pattern)) {
-		return true;
+	public static boolean firstName(String name) {
+		String Firstname = "[A-Z]{1}[a-z]{3}";
+		if(name.matches(Firstname)) {
+			return true;
+		}
+		return false;
+			
 	}
-	return false;
-}
+	public static boolean lastName(String lastName) {
+		String Lastname = "[A-Z]{1}[a-z]{3}";
+		if(lastName.matches(Lastname)) {
+			return true;
+		}
+		return false;
+		
+	}
+
+	public static boolean emailId(String emailId) {
+		String Emailid = "[abc](.+){1}[A-Za-z]*+@{1}[bl](.+)[co](.+)[in]$";
+		if(emailId.matches(Emailid)) {
+			return true;
+		}
+		return false;
+	}
+	public static boolean mobileNumber(String mobileNumber) {
+		String Mobilenumber = "^(91)\\s{1}[7-9]{1}[0-9]{9}$";
+		if(Mobilenumber.matches(Mobilenumber)) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean password(String password) {
+		String pattern = "(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}";
+		if(password.matches(pattern)) {
+			return true;
+		}
+		return false;
+	}
 
 
 }
